cfc5abb07009bf8d21937a6d0b573f51ae613b34
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-09-24 23:57:00 +0530

    changes to be committed

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const jwtAuth = require('../Middleware/userMiddleware.js')
-const {signup, signin, login} = require('../Controller/userController.js')
+const {
+    signup,
+    signin,
+    forgotPassword,
+    resetPassword,
+    login,
+    logout
+  } = require("../Controller/userController.js");
 
 router.post('/signup',signup);
 router.post('/signin',signin);
+
+router.post("/forgotpassword", forgotPassword);
+router.post("/resetpassword/:token", resetPassword);
+
+router.get("/user", jwtAuth, login);
+router.get("/logout", jwtAuth, logout);
 
 router.get('/login',login);
 module.exports = router;
